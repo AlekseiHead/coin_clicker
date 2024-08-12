0bce6219f5b1ccd34a1b75830cbf2eb84a983578
Aleksei Golovanev <head.dev22@gmail.com>
2024-08-12 18:59:07 +0300

    change points

--- src/app.js
+++ src/app.js
@@ -16,17 +16,17 @@ function setScore(score) {
 
 function setImage() {
 	
-	if (getScore() >= 150) {
+	if (getScore() >= 450) {
 		$circle.setAttribute('src', './assets/cat2.png')
 		document.getElementById('footer_level').textContent = "Intermediate";
-		document.getElementById('coins').textContent = "400";
+		document.getElementById('coins').textContent = "2000";
 	}
-	if (getScore() >= 400) {
+	if (getScore() >= 2000) {
 		$circle.setAttribute('src', './assets/cat3.png')
 		document.getElementById('footer_level').textContent = "Advanced";
-		document.getElementById('coins').textContent = "800";
+		document.getElementById('coins').textContent = "5000";
 	}
-	if (getScore() >= 800) {
+	if (getScore() >= 5000) {
 		$circle.setAttribute('src', './assets/cat4.png')
 		document.getElementById('footer_level').textContent = "Proficiency";
 		document.getElementById('coins').textContent = "∞";
